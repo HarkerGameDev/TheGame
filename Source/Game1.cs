d311e639c5bb4bdd551bd2b98e9928866a17daf5
Schwo0ps <Schwo0ps@users.noreply.github.com>
2015-10-14 18:18:20 -0700

    Dynamic Camera -- more down Suddenly, with just a bit of camera movement, you can start to feel the action in the game. Just some proper levels, more constant tweaking, and basic art are needed before proper playtesting can begin. This is coming together quite nicely.

--- Source/Game1.cs
+++ Source/Game1.cs
@@ -46,22 +46,28 @@ namespace Source
         private Random rand;
         private World world;
 
+        private const float CAMERA_SCALE = 30f;
+        private const float MAX_CAMERA_SPEED_X = 6f;
+        private const float MAX_CAMERA_SPEED_Y = 4f;
+        private Rectangle cameraBounds;
+        private Vector2 screenCenter;
+
 		private bool paused;
 		private Player player;
-        private List<Item> floors;
+        private List<Floor> floors;
         
-		class Item
+		class Floor
 		{
             public Body Body;
             public Vector2 Origin;
             public Vector2 Scale;
 
-            protected Item()
+            protected Floor()
             {
                 Origin = new Vector2(0.5f, 0.5f);
             }
             
-            public Item(World world, Vector2 position1, Vector2 position2)
+            public Floor(World world, Vector2 position1, Vector2 position2)
             {
                 Vector2 dist = position2 - position1;
                 float width = dist.Length();
@@ -82,7 +88,7 @@ namespace Source
             }
 		}
 
-        class Player : Item
+        class Player : Floor
         {
             public bool CanJump
             {
@@ -149,16 +155,17 @@ namespace Source
             graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width / 2;
             graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height / 2;
             graphics.IsFullScreen = false;
+            IsMouseVisible = true;
             graphics.ApplyChanges();
 
             // Sets how many pixels is a meter for Farseer
             ConvertUnits.SetDisplayUnitToSimUnitRatio(32f);
 
             // Create objects
-            world = new World(new Vector2(0, 13f));
+            world = new World(new Vector2(0, 13f));     // gravity in N
             rand = new Random();
             player = new Player(world);
-            floors = new List<Item>();
+            floors = new List<Floor>();
 
             // Set variables
             paused = false;
@@ -180,6 +187,12 @@ namespace Source
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            // Initialize camera
+            int width = graphics.GraphicsDevice.Viewport.Width;
+            int height = graphics.GraphicsDevice.Viewport.Height;
+            cameraBounds = new Rectangle((int)(width * 0.15), (int)(height * 0.25), (int)(width * 0.3), (int)(height * 0.5));
+            screenCenter = cameraBounds.Center.ToVector2();
+
 			// Use this to draw rectangles
 			whiteRect = new Texture2D(GraphicsDevice, 1, 1);
 			whiteRect.SetData(new[] { Color.White });
@@ -190,13 +203,13 @@ namespace Source
 
             // Farseer level hard-coding
             // TODO load some file instead of hardcoding
-            floors.Add(new Item(world, new Vector2(1f, 15f), new Vector2(29f, 15f)));
-            floors.Add(new Item(world, new Vector2(1f, 9f), new Vector2(12f, 9f)));
-            floors.Add(new Item(world, new Vector2(18f, 9f), new Vector2(29f, 9f)));
-            floors.Add(new Item(world, new Vector2(8f, 13f), new Vector2(15f, 10f)));
-            floors.Add(new Item(world, new Vector2(1f, 3f), new Vector2(12f, 3f)));
-            floors.Add(new Item(world, new Vector2(18f, 3f), new Vector2(29f, 3f)));
-            floors.Add(new Item(world, new Vector2(8f, 7f), new Vector2(15f, 4f)));
+            floors.Add(new Floor(world, new Vector2(1f, 15f), new Vector2(29f, 15f)));
+            floors.Add(new Floor(world, new Vector2(1f, 9f), new Vector2(12f, 9f)));
+            floors.Add(new Floor(world, new Vector2(18f, 9f), new Vector2(29f, 9f)));
+            floors.Add(new Floor(world, new Vector2(8f, 13f), new Vector2(15f, 10f)));
+            floors.Add(new Floor(world, new Vector2(1f, 3f), new Vector2(12f, 3f)));
+            floors.Add(new Floor(world, new Vector2(18f, 3f), new Vector2(29f, 3f)));
+            floors.Add(new Floor(world, new Vector2(8f, 7f), new Vector2(15f, 4f)));
 		}
 
 		/// <summary>
@@ -252,6 +265,7 @@ namespace Source
 
             //float impulse = 0.5f * (2 - Math.Abs(player.Body.LinearVelocity.X) / MAX_VELOCITY);
             float impulse = MathHelper.SmoothStep(0.5f, 0f, Math.Abs(player.Body.LinearVelocity.X) / MAX_VELOCITY);
+            impulse = (float)Math.Pow(impulse, 0.5);
 
             if (state.IsKeyDown(Keys.Right))
             {
@@ -279,6 +293,21 @@ namespace Source
                 player.JumpWait = JUMP_WAIT;
                 player.Body.ApplyLinearImpulse(new Vector2(0f, -13f));
             }
+
+            //screenCenter.X = MathHelper.SmoothStep(cameraBounds.Left, cameraBounds.Right, player.Body.LinearVelocity.X / MAX_VELOCITY / 2f + 0.5f);
+            //screenCenter.Y = MathHelper.SmoothStep(cameraBounds.Top, cameraBounds.Bottom, player.Body.LinearVelocity.Y / MAX_VELOCITY / 2f + 0.5f);
+
+            // TODO ever so slight camera shake when going fast
+            float deltaX = ((cameraBounds.Center.X - screenCenter.X) / cameraBounds.Width - player.Body.LinearVelocity.X / MAX_VELOCITY) * CAMERA_SCALE;
+            deltaX = MathHelper.Clamp(deltaX, -MAX_CAMERA_SPEED_X, MAX_CAMERA_SPEED_X);
+            screenCenter.X += deltaX;
+            screenCenter.X = MathHelper.Clamp(screenCenter.X, cameraBounds.Left, cameraBounds.Right);
+            //Console.WriteLine(string.Format("deltaX: {0}", deltaX));
+
+            float deltaY = ((cameraBounds.Center.Y - screenCenter.Y) / cameraBounds.Height - player.Body.LinearVelocity.Y / MAX_VELOCITY) * CAMERA_SCALE;
+            deltaY = MathHelper.Clamp(deltaY, -MAX_CAMERA_SPEED_Y, MAX_CAMERA_SPEED_Y);
+            screenCenter.Y += deltaY;
+            screenCenter.Y = MathHelper.Clamp(screenCenter.Y, cameraBounds.Top, cameraBounds.Bottom);
         }
 
         private void CheckPlayer()
@@ -298,10 +327,13 @@ namespace Source
             int width = GraphicsDevice.Viewport.Width;
             int height = GraphicsDevice.Viewport.Height;
 
+            // Calculate camera location matrix
+            Matrix view = Matrix.CreateTranslation(new Vector3(screenCenter - ConvertUnits.ToDisplayUnits(player.Body.Position), 0f));
+
             // Draw player and floors
-			spriteBatch.Begin();
+            spriteBatch.Begin(transformMatrix: view);
             spriteBatch.Draw(whiteRect, ConvertUnits.ToDisplayUnits(player.Body.Position), null, Color.Red, player.Body.Rotation, player.Origin, player.Scale, SpriteEffects.None, 0f);
-			foreach (Item item in floors)
+			foreach (Floor item in floors)
 			{
                 spriteBatch.Draw(whiteRect, ConvertUnits.ToDisplayUnits(item.Body.Position), null, Color.Azure, item.Body.Rotation, item.Origin, item.Scale, SpriteEffects.None, 0f);
 			}
