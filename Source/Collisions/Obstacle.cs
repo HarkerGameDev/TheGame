7f480b5b6c40c8b2d5c7625e0f9642e79949a9da
Schwo0ps <Schwo0ps@users.noreply.github.com>
2015-11-18 12:25:54 -0800

    Added Obstacle.cs

---
+++ Source/Collisions/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Source.Collisions
+{
+    /// <summary>
+    /// An obstacle is a thing the player must jump over
+    /// </summary>
+    public class Obstacle : Body
+    {
+        public const float OBSTACLE_WIDTH = 0.8f;
+        public const float OBSTACLE_HEIGHT = 0.5f;
+        //public bool Breakable = false;
+
+        public Obstacle(Texture2D texture, Vector2 position, float scale = 1f)
+            : base(texture, position, new Vector2(OBSTACLE_WIDTH, OBSTACLE_HEIGHT) * scale)
+        {
+            Color = Color.DarkSlateBlue;
+        }
+    }
+}
