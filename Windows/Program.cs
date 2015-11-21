cb55700503909f92aee5fc9e10bf26b86c5b6254
Schwo0ps <Schwo0ps@users.noreply.github.com>
2015-11-21 02:53:54 -0800

    Fixed auto-update

--- Windows/Program.cs
+++ Windows/Program.cs
@@ -17,35 +17,39 @@ namespace Game
         static void Main()
         {
             // Attempt to auto update
-            string tempFile = "Update.msi";
-            new System.Net.WebClient().DownloadFile("https://github.com/HarkerGameDev/TheGame/raw/master/install/GameInstaller.msi", tempFile);
-            
-            //Installer installer = (Installer)Activator.CreateInstance(Type.GetTypeFromProgID("WindowsInstaller.Installer"));
-            //Database db = installer.OpenDatabase(tempFile, 0);
-            //View dv = db.OpenView("SELECT `Value` FROM `Property` WHERE `Property`='ProductVersion'");
-            //dv.Execute();
-            //Record record = dv.Fetch();
-            //string str = record.get_StringData(1);
-            //dv.Close();
-
-            using (Database db = new Database(tempFile))
+            try
             {
-                View dv = db.OpenView("SELECT `Value` FROM `Property` WHERE `Property`='ProductVersion'");
-                dv.Execute();
-                Record record = dv.Fetch();
-                string str = record.GetString(1);
-                dv.Close();
-                db.DeleteOnClose(tempFile);
+                string tempFile = "Update.msi";
+                new System.Net.WebClient().DownloadFile("https://github.com/HarkerGameDev/TheGame/raw/master/install/GameInstaller.msi", tempFile);
 
-                Version newVer = new Version(str);
-                Version curVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-                if (newVer > curVer)
+                //Installer installer = (Installer)Activator.CreateInstance(Type.GetTypeFromProgID("WindowsInstaller.Installer"));
+                //Database db = installer.OpenDatabase(tempFile, 0);
+                //View dv = db.OpenView("SELECT `Value` FROM `Property` WHERE `Property`='ProductVersion'");
+                //dv.Execute();
+                //Record record = dv.Fetch();
+                //string str = record.get_StringData(1);
+                //dv.Close();
+
+                using (Database db = new Database(tempFile))
                 {
-                    //Console.WriteLine(newVer + "\n" + curVer);
-                    //Installer.SetInternalUI(InstallUIOptions.Basic);
-                    Installer.InstallProduct(tempFile, "");
+                    View dv = db.OpenView("SELECT `Value` FROM `Property` WHERE `Property`='ProductVersion'");
+                    dv.Execute();
+                    Record record = dv.Fetch();
+                    string str = record.GetString(1);
+                    dv.Close();
+                    db.DeleteOnClose(tempFile);
+
+                    Version newVer = new Version(str);
+                    Version curVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                    if (newVer > curVer)
+                    {
+                        //Console.WriteLine(newVer + "\n" + curVer);
+                        //Installer.SetInternalUI(InstallUIOptions.Basic);
+                        Installer.InstallProduct(tempFile, "");
+                    }
                 }
             }
+            catch (Exception e) { }
             
             //System.IO.File.Delete(tempFile);
 
